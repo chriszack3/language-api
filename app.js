e395e445643d494590ebd6d7bb37dbb9002eddb7
chriszack3 <chriszack37377@gmail.com>
2022-10-11 12:41:33 -0400

    refactor working locally

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from 'express'
-import runScraper from './lib/functions/getComments.js'
+import getLangResults from './lib/functions/languageLib.js'
+import dotenv from 'dotenv'
 
 const app = express()
 const port = process.env.PORT || 3000
+dotenv.config()
+console.log(process.env)
 
 
 app.get('/', async (req, res) => {
-    runScraper().then((data) => {
+  getLangResults(req?.body?.text || 'Undefined on the server').then((data) => {
         res.send({ data: data })
     }).catch((err) => {
         console.log(err)
