6d9f452306dade86c83ebbf02e134f40cf5eeeeb
chriszack3 <chriszack37377@gmail.com>
2022-10-11 17:30:03 -0400

    mixed up express and axios syntax...

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ import bodyParser from 'body-parser'
 const app = express()
 const port = process.env.PORT || 3000
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(bodyParser.json())
 createToken()
 
-app.post('/', urlencodedParser, async (req, res) => {
+app.post('/', async (req, res) => {
   getLangResults(req?.body?.text || 'Undefined on the server').then((data) => {
-    res.header("Access-Control-Allow-Origin", "localhost:8000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://localhost:8000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.status(200).json(data)
   }).catch((err) => {
