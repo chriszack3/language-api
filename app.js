bbdb47a9609e94cbe7a29ff7de663785f7e9d212
chriszack3 <chriszack37377@gmail.com>
2022-10-11 18:05:45 -0400

    parse the json

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({extended:false}));
 app.post('/', async (req, res) => {
-  console.log(req.body);
-  const result = await getLangResults(req.body.text)
+  console.log(req?.body);
+  const result = await getLangResults(JSON.parse(req?.body?.text));
   res.header('Access-Control-Allow-Origin', 'http://localhost:8000')
   res.status(200).json(result)
   res.end();
