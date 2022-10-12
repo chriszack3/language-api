e689d56c509ff98ba292daadff8226cc86067d37
chriszack3 <chriszack37377@gmail.com>
2022-10-12 15:18:22 -0400

    added back localhost

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const port = process.env.PORT || 3000
 app.use(express.urlencoded({extended:false}));
 app.post('/', async (req, res) => {
   const result = await getLangResults(req?.body?.text || 'Nothing passed to api');
-  res.header('Access-Control-Allow-Origin', 'https://build-9ae1cebc-2bdd-4a6c-bf05-5a6bd965f64d.gtsb.io')
+  //allow the domain and localhost to access the api
+  res.header('Access-Control-Allow-Origin', ['https://build-9ae1cebc-2bdd-4a6c-bf05-5a6bd965f64d.gtsb.io', 'http://localhost:8000'])
   res.status(200).json(result)
   res.end();
 });
