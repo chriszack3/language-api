378c79b0a257555f5a3d39107398ccacb9e25ef2
chriszack3 <czack@chrispc.localdomain>
2022-11-22 12:54:17 -0500

    refactor for cors

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ createToken()
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.post('/', async (req, res) => {
   const result = await getLangResults(req?.body?.text || 'Nothing passed to api');
   //allow the domain and localhost to access the api
-  res.header('Access-Control-Allow-Origin', '*')
   res.status(200).json(result)
   res.end();
 });
